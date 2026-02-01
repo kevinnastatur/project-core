1bbcaafa67e170f0dfc8234b5dd49ad70e8193c0
kevinnastatur <kevinnastatur@yahoo.com>
2026-02-01 19:37:04 +0700

    Redirect root page to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <div className="flex min-h-screen items-center justify-center "></div>;
+  redirect("/auth/login");
 }
